0046762e8c877b4cce71005b1ff46135e138c7e8
Subramaniyajothi6 <subramaniyajothi2002@gmail.com>
2025-12-10 18:42:40 +0530

    final touch

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(cors({
   origin: "http://127.0.0.1:5500",
-  methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
