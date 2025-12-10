0fdc112ba714a491ad3badb7ca9104644211afcd
Subramaniyajothi6 <subramaniyajothi2002@gmail.com>
2025-12-10 16:32:49 +0530

    final touch

--- backend/config/mysql.js
+++ backend/config/mysql.js
@@ -1,20 +1,14 @@
+import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 dotenv.config();
-import mysql from "mysql2";
 
-const connection = mysql.createConnection({
+export const mysqlPool = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DB,
   port: process.env.MYSQL_PORT,
-  ssl: { rejectUnauthorized: false }
-});
-
-connection.connect((err) => {
-  if (err) {
-    console.error("MySQL Error:", err);
-    return;
+  ssl: {
+    rejectUnauthorized: false
   }
-  console.log("MySQL Connected");
 });
